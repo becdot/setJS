f7700dbf9c0c101de72f2289952b9e9ae0eba56c
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-03-27 17:00:35 -0400

    Changed != to !== for safety.
    * != Tries type coercion, which can produce unexpected results.  For example: "1" == 1 but "1" !== 1

--- cards.js
+++ cards.js
@@ -147,7 +147,7 @@ Table.prototype.clickCard = function(card) {
 }
 Table.prototype.unclickCard = function(card) {
     var index = this.clickedCards.indexOf(card);
-    if (index != -1)
+    if (index !== -1)
         this.clickedCards.splice(index, 1);
 }
 Table.prototype.unclickAllCards = function() {

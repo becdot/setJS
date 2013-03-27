49cb75ba1cc7e9bdff03c58aa8de3c33786380eb
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-03-27 17:08:09 -0400

    Stop i, j and k from being instantiated as globals.

--- cards.js
+++ cards.js
@@ -162,7 +162,6 @@ Table.prototype.iterateTable = function(state) {
     if (state === false) {
         return { "result": false, "state": false};
     }
-<<<<<<< HEAD
     if(typeof state === "undefined") {
         i = 0;
         j = 1;
